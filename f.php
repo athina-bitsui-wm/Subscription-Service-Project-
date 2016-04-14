812be0ee11313eeda277d836695384ade189fc58
athina bitsui <athina.bitsui@west-mec.org>
2016-04-14 15:25:20 -0700

    hotaru

--- f.php
+++ f.php
@@ -1,3 +1,20 @@
-<?php
+<!DOCTYPEhtml>
+<html>
+<head>
+    <title>
+        ~ book review ~
+    </title>
+</head>
 
+<body>
+<?php
+$ratting = $_POST['ratting'];
+$comment = $_POST['comment'];
+$choices = $_POST['choices'];
+
+echo 'how good is the book'. $ratting .'<br />';
+echo 'write a summry'. $comment .'<br />';
+echo 'would you like to recomed this to a friend or family member?'. $choices . '<br />';
 ?>
+</body>
+</html>
